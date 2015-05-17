57adb8603ceaa907a968eef4fa54adca35dbb436
a847575422 <1299706476@qq.com>
2015-05-17 17:36:47 +0800

    test

--- org.cloudbus.cloudsim/QCloudlet.java
+++ org.cloudbus.cloudsim/QCloudlet.java
@@ -4,10 +4,36 @@
  * and open the template in the editor.
  */
 
+import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.UtilizationModel;
+
 /**
  *
  * @author YAO
  */
-public class QCloudlet {
-    
+public class QCloudlet extends Cloudlet {
+
+    private double waitingTime;
+
+    public QCloudlet(int cloudletId, long cloudletLength, int pesNumber, long cloudletFileSize, long cloudletOutputSize, UtilizationModel utilizationModelCpu, UtilizationModel utilizationModelRam, UtilizationModel utilizationModelBw) {
+        super(cloudletId, cloudletLength, pesNumber, cloudletFileSize, cloudletOutputSize, utilizationModelCpu, utilizationModelRam, utilizationModelBw);
+    }
+
+    /**
+     * Get the value of waitingTime
+     *
+     * @return the value of waitingTime
+     */
+    public double getWaitingTime() {
+        return waitingTime;
+    }
+
+    /**
+     * Set the value of waitingTime
+     *
+     * @param waitingTime new value of waitingTime
+     */
+    public void setWaitingTime(double waitingTime) {
+        this.waitingTime = waitingTime;
+    }
 }
