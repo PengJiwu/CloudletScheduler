33fab67fa6e63f0f452bfa6599bf8e6015eda84a
a847575422 <1299706476@qq.com>
2015-05-21 21:14:18 +0800

    DatacenterBroker submitCloudlets

--- src/org/cloudbus/cloudsim/VmCloudletAssignerRandom.java
+++ src/org/cloudbus/cloudsim/VmCloudletAssignerRandom.java
@@ -20,8 +20,14 @@ public class VmCloudletAssignerRandom extends VmCloudletAssigner {
 			for (QCloudlet cloudlet : cloudletList)
 				cloudlet.setSubmittedTime(sumittedTime);
 
+			List<QCloudlet> toAssignCloudletList = new ArrayList<QCloudlet>();
+			if (getGlobalCloudletWaitingQueue() != null
+					&& getGlobalCloudletWaitingQueue().size() != 0)
+				toAssignCloudletList.addAll(getGlobalCloudletWaitingQueue());
+			toAssignCloudletList.addAll(cloudletList);
+
 			int m = vmList.size();
-			int n = cloudletList.size();
+			int n = toAssignCloudletList.size();
 			int maxCloudletsWaitingLength = ((QCloudletSchedulerSpaceShared) vmList
 					.get(0).getCloudletScheduler())
 					.getCloudletWaitingQueueLength();
@@ -37,26 +43,10 @@ public class VmCloudletAssignerRandom extends VmCloudletAssigner {
 				vmWaitingQueueSizeList.add(queueSize);
 			}
 
-			int randnVmIndex[] = new int[n];
-			for (int i = 0; i < n; i++)
-				randnVmIndex[i] = randomInt(0, m);
-
 			for (int i = 0; i < n; i++) {
-				int mSize = vmWaitingQueueSizeList.get(randnVmIndex[i]).get(
-						"size");
-				if (mSize <= maxCloudletsWaitingLength) {
-					vmWaitingQueueSizeList.get(randnVmIndex[i]).put("size",
-							mSize++);
-					cloudletList.get(i).setVmId(
-							vmList.get(randnVmIndex[i]).getId());
-					randnVmIndex[i] = -1; // 已经使用
-				}
-
-			}			
-
-			// 还有没有分配VM的的任务
-			for (int i = 0; i < n; i++) {
-				if (cloudletList.get(i).getVmId() != -1) {
+				int index = randomInt(0, m);
+				int mSize = vmWaitingQueueSizeList.get(index).get("size");
+				if (mSize >= maxCloudletsWaitingLength) {//若随机的队列满了，往最空的队列
 					// 根据等待队列大小对vmlist排序,从小到大
 					Collections.sort(vmWaitingQueueSizeList,
 							new Comparator<Map<String, Integer>>() {
@@ -66,23 +56,41 @@ public class VmCloudletAssignerRandom extends VmCloudletAssigner {
 											.get("size")));
 								}
 							});
-					//等待队列最空的
-					int mSize = vmWaitingQueueSizeList.get(0).get("size");
-					if (mSize < maxCloudletsWaitingLength) {
-						cloudletList.get(i).setVmId(
-								vmList.get(
-										vmWaitingQueueSizeList.get(0).get(
-												"index")).getId());
-						vmWaitingQueueSizeList.get(i).put("size", mSize++);
-					} else	//全都满了
+					index = 0;
+					mSize = vmWaitingQueueSizeList.get(0).get("size");
+					if (mSize >= maxCloudletsWaitingLength)
 						break;
 				}
+
+				vmWaitingQueueSizeList.get(index).put("size", mSize++);
+				toAssignCloudletList.get(i).setVmId(
+						vmList.get(
+								vmWaitingQueueSizeList.get(index).get("index"))
+								.getId());
+
 			}
 
+			/*
+			 * // 还有没有分配VM的的任务 for (int i = 0; i < n; i++) { if
+			 * (toAssignCloudletList.get(i).getVmId() != -1) { //
+			 * 根据等待队列大小对vmlist排序,从小到大 Collections.sort(vmWaitingQueueSizeList,
+			 * new Comparator<Map<String, Integer>>() { public int
+			 * compare(Map<String, Integer> queue1, Map<String, Integer> queue2)
+			 * { return (queue1.get("size").compareTo(queue2 .get("size"))); }
+			 * }); // 等待队列最空的 int mSize =
+			 * vmWaitingQueueSizeList.get(0).get("size"); if (mSize <
+			 * maxCloudletsWaitingLength) { toAssignCloudletList.get(i).setVmId(
+			 * vmList.get( vmWaitingQueueSizeList.get(0).get(
+			 * "index")).getId()); vmWaitingQueueSizeList.get(i).put("size",
+			 * mSize++); } else // 全都满了 break; } }
+			 */
+
 			// 所有Vm的CloudletWaitingQueue都满了
+			getGlobalCloudletWaitingQueue().clear();
 			for (int i = 0; i < n; i++) {
-				if (cloudletList.get(i).getVmId() != -1) {
-					getGlobalCloudletWaitingQueue().offer(cloudletList.get(i));
+				if (toAssignCloudletList.get(i).getVmId() == -1) {
+					getGlobalCloudletWaitingQueue().offer(
+							toAssignCloudletList.get(i));
 				}
 			}
 
